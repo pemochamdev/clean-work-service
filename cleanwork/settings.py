b39f0efcc69609abf46b43416b73215d8126b532
pemochamdev <pemochamdev@gmail.com>
2024-05-15 09:34:40 -0400

    creation de 2 appli

--- cleanwork/settings.py
+++ cleanwork/settings.py
@@ -129,7 +129,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-AUTH_USER_MODEL = "accounts.User"
+#AUTH_USER_MODEL = "accounts.User"
 
 # SETTINGS EMAIL
 
